851d9d41701c80b4e49b7817b89be759c3a5a37f
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-12 22:53:54 -0400

    check App.js to see about jsx

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -5,16 +5,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+        //this is jsx code , not html
+        // we use className instead of class, cause 'class' is a reserve word in Js
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          111To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>another header</h1>
       </div>
     );
+    //jsx compiles to the code below
+    //return React.createElement('div', {className: 'App'}, 'first text', 'second text', React.createElement('h1', null, 'third inner h1 text'));
+
   }
 }
 
