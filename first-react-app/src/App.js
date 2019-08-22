0fb1b896cb0011f277a3e07da5fd1dc12c2da2ce
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-21 20:22:59 -0400

    add state and prop pdf

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,29 +3,28 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    // state can only be accessed in class-based components!
+    state = {
+        persons: [
+            {name: "tian", age: "31" },
+            {name: "chris", age: "23" },
+            {name: "mike", age: "21" }
+          ]
+    }
 
-  state = {
-      persons: [
-          {name: "tian", age: "31" },
-          {name: "chris", age: "23" },
-          {name: "mike", age: "21" }
-      ]
-  }
-
-  render() {
-    return (
-        //this is jsx code , not html
-        // we use className instead of class, cause 'class' is a reserve word in Js
-      <div className="App">
-        <h1>header</h1>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
-    );
-    //jsx compiles to the code below
-    //return React.createElement('div', {className: 'App'}, 'first text', 'second text', React.createElement('h1', null, 'third inner h1 text'));
-
+    render() {
+        return (
+            //this is jsx code , not html
+            // we use className instead of class, cause 'class' is a reserve word in Js
+          <div className="App">
+            <h1>header</h1>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          </div>
+        );
+        //jsx compiles to the code below
+        //return React.createElement('div', {className: 'App'}, 'first text', 'second text', React.createElement('h1', null, 'third inner h1 text'));
   }
 }
 
