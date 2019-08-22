58ba9aa11d65108ab691062976f31ed2cd8b1bd7
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-21 20:14:33 -0400

    37. Understanding & Using State

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+      persons: [
+          {name: "tian", age: "31" },
+          {name: "chris", age: "23" },
+          {name: "mike", age: "21" }
+      ]
+  }
+
   render() {
     return (
         //this is jsx code , not html
         // we use className instead of class, cause 'class' is a reserve word in Js
       <div className="App">
         <h1>header</h1>
-        <Person name="tian" age="31"/>
-          <Person name="chris" age="23">My hobbies: racing</Person>
-        <Person name="mike" age="21"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //jsx compiles to the code below
