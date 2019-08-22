92f39f470efa41628fd928c0ac113e63ba0552f9
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-21 22:23:21 -0400

    47. Adding Styling with Stylesheets

--- first-react-app/src/Person/Person.js
+++ first-react-app/src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css'
 
 //a component is just a js function return jsx
 const person = (props) => {
     return (
-        <div>
+        <div className='Person'>
             <p onClick={props.passClick}> My name is {props.name} and I'm {props.age} years old !</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changeName} value={props.name}/>
