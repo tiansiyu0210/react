e70d52d0c3070e8108e4cfbc274f5cfbeff5572d
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-21 21:37:18 -0400

    43. Using the useState() Hook for State Manipulation

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,47 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-    // state can only be accessed in class-based components!
-    state = {
+const app = props => {
+    const [personState, setPersonState] = useState({
         persons: [
-            {name: "tian", age: "31" },
-            {name: "chris", age: "23" },
-            {name: "mike", age: "21" }
-          ]
-    }
+            {name: "tian", age: "31"},
+            {name: "chris", age: "23"},
+            {name: "mike", age: "21"}
+        ]
+    });
 
-    switchNameHandler =  () => {
-        console.log("clicked");
-        this.setState({
+
+    const [otherState, setOtherState] = useState({
+        other: "some other state"
+    });
+
+
+    console.log(personState, otherState);
+
+    const switchNameHandler = () => {
+        setPersonState({
             persons: [
-                {name: "siyu", age: "34" },
-                {name: "shan", age: "22" },
-                {name: "jj", age: "23" }
+                {name: "siyu", age: "34"},
+                {name: "shan", age: "22"},
+                {name: "jj", age: "23"}
             ]
-        })
-    }
+        });
+    };
 
-    render() {
-        return (
-            //this is jsx code , not html
-            // we use className instead of class, cause 'class' is a reserve word in Js
-          <div className="App">
+
+    return (
+        <div className="App">
             <h1>header</h1>
             {/* https://reactjs.org/docs/events.html#supported-events */}
-            <button onClick={this.switchNameHandler}>switch name</button>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-          </div>
-        );
-        //jsx compiles to the code below
-        //return React.createElement('div', {className: 'App'}, 'first text', 'second text', React.createElement('h1', null, 'third inner h1 text'));
-  }
+            <button onClick={switchNameHandler}>switch name</button>
+            <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
+            <Person name={personState.persons[1].name} age={personState.persons[1].age}>My hobbies: racing</Person>
+            <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+        </div>
+    );
+    //jsx compiles to the code below
+    //return React.createElement('div', {className: 'App'}, 'first text', 'second text', React.createElement('h1', null, 'third inner h1 text'))
 }
 
-export default App;
+export default app;
 
 
 
