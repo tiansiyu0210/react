70de3b02510daeb61b0353643a5ed63c6d830213
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-25 21:03:28 -0400

    84. A Better Project Structure

--- first-react-app/src/containers/App.js
+++ first-react-app/src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import appClasses from './App.module.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component{
     // state can only be accessed in class-based components!
