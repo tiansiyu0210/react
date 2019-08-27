edd5f49b1c52be9db76d805a38e0f0308dceb47c
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-27 12:17:11 -0400

    99. PureComponents instead of shouldComponentUpdate

--- first-react-app/src/components/Persons/Persons.js
+++ first-react-app/src/components/Persons/Persons.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from "./Person/Person";
 
 
-class Persons extends Component {
+class Persons extends PureComponent {
     static getDerivedStateFromProps(props, state){
         console.log('[Persons.js] getDerivedStateFromProps');
         return state;
     }
 
-    shouldComponentUpdate(nextProps, nextState){
-        console.log('[Persons.js] shouldComponentUpdate');
-        return nextProps.persons !== this.props.persons
-    }
+    // shouldComponentUpdate(nextProps, nextState){
+    //     console.log('[Persons.js] shouldComponentUpdate');
+    //     return nextProps.persons !== this.props.persons
+    // }
 
     getSnapshotBeforeUpdate(pervProps, prevState){
         console.log('[Persons.js] getSnapshotBeforeUpdate');
