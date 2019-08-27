284e398661ca5188684dd42f8a501a34e63ff22b
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-27 12:44:42 -0400

    103. Using React.Fragment

--- first-react-app/src/components/Persons/Person/Person.js
+++ first-react-app/src/components/Persons/Person/Person.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import classes from './Person.module.css';
-import Aux from '../../../hoc/Aux';
 
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <Aux className={classes.Person}>
+            <React.Fragment>
                 <p onClick={this.props.click}> My name is {this.props.name} and I'm {this.props.age} years old !</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changeName} value={this.props.name}/>
-            </Aux>
+            </React.Fragment>
         );
     }
 }
