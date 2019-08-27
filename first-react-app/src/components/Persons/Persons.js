d2cfea37afef27ffec01bd9d05407e70c855527d
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-26 23:09:47 -0400

    96. Using shouldComponentUpdate for Optimization

--- first-react-app/src/components/Persons/Persons.js
+++ first-react-app/src/components/Persons/Persons.js
@@ -10,7 +10,7 @@ class Persons extends Component {
 
     shouldComponentUpdate(nextProps, nextState){
         console.log('[Persons.js] shouldComponentUpdate');
-        return true;
+        return nextProps.persons !== this.props.persons
     }
 
     getSnapshotBeforeUpdate(pervProps, prevState){
