73191983bc14c1dd9de8e9634186b0713828fbd3
tiansiyu0210 <tiansiyu297@gmail.com>
2019-08-27 21:14:18 -0400

    112. Using the Context API

--- first-react-app/src/containers/App.js
+++ first-react-app/src/containers/App.js
@@ -52,7 +52,11 @@ class App extends Component{
     }
 
     loginHandler = () => {
-        this.setState({auth: true})
+        this.setState((prevState, props) => {
+            return {
+                auth: !prevState.auth
+            };
+        });
     }
 
 
